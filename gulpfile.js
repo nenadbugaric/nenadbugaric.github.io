35da9459fed00b6f3f881b28eafe5ffe0d98293b
Nenad Bugaric <n.bugaric@gmail.com>
2015-07-28 16:44:31 +0200

    add projects content and fix blue_background

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,13 @@
 var gulp = require('gulp'),
-    sass = require('gulp-sass');
+    sass = require('gulp-sass'),
+    watch = require('gulp-watch');
 
 gulp.task('sass', function(){
   return gulp.src('assets/sass/main.scss')
         .pipe(sass({ outputStyle: 'compressed' }))
-        //.pipe(sass({ sourceComments: 'map' }))
         .pipe(gulp.dest('assets/css'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('assets/sass/main.scss', ['sass']);
 });
